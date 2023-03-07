9e4b1b720e3e5730d246beebe2b0f925ec9c3ec4
Asher Berryman <ashberr@chbc.school.nz>
2023-03-07 13:15:00 +1300

    version 2 -works as expected with expected, boundry, invalid case data

--- WagesApp/Program.cs
+++ WagesApp/Program.cs
@@ -8,11 +8,49 @@ namespace WagesApp
     {
 
         //global variables
-
+        static string topEarner = "";
+        static int topEarnerHours = -1;
 
         //methods and/or functions
 
-        static void CalculateWages(int totalHoursWorked)
+        static string CheckFlag()
+        {
+            while (true)
+            {
+                //get users choice
+                Console.WriteLine("Press <ENTER> to add another employee or type 'XXX' to quit\n");
+                string userInput = Console.ReadLine();
+
+                //Convert user input to uppercase
+                userInput = userInput.ToUpper();
+
+                if (userInput.Equals("XXX") ||userInput.Equals(""))
+                {
+                    return userInput;
+                }
+                Console.WriteLine("Error: Please enter a correct choice.");
+            }
+        }
+
+        static string CheckName()
+        {
+            while (true)
+            {
+                //get name
+                Console.WriteLine("Enter the employee's name:\n");
+                string name = Console.ReadLine();
+
+                if (!name.Equals(""))
+                {
+                    //convert employee name to capitalised name
+                    name = name[0].ToString().ToUpper() + name.Substring(1);
+                    return name;
+                }
+                Console.WriteLine("Error: You must enter a name for the employee");
+            }
+        }
+
+            static void CalculateWages(int totalHoursWorked, string employeeName)
         {
             //Display the total weekly hours stored
             Console.WriteLine($"Total hours worked : {totalHoursWorked}hrs");
@@ -27,17 +65,32 @@ namespace WagesApp
                 Console.WriteLine($"5 bonus hours added: {totalHoursWorked}hrs");
             }
 
+            if (totalHoursWorked > topEarnerHours)
+            {
+                topEarnerHours = totalHoursWorked;
+                topEarner = employeeName;
 
-
+            }
             //Calculate wage at a rate of $22/hr
 
             int wages = totalHoursWorked * 22;
 
-            //Calculate tax
+            float tax = 0.07f;
 
+            //Calculate tax
+            if (wages > 450)
+            {
+                tax = 0.08f;
+            }
             //Calculate final pay
 
+            float finalPay = wages - (float)Math.Round(wages * tax, 2);
+
             //Display the results of the calculations followed by two blank lines
+            Console.WriteLine($"Weekly Pay: {wages}\n" + 
+                $"Tax Rate: {tax}\n" + 
+                $"Tax: {Math.Round(wages * tax, 2)}\n " +
+                $"Final Pay: {finalPay}\n\n\n");
         }
 
         static void OneEmployee()
@@ -45,8 +98,8 @@ namespace WagesApp
             List<string> weekDays = new List<string>() { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday" };
 
             //Enter and store employee name
-            Console.WriteLine("Enter Employee Name:\n");
-            string employeeName = Console.ReadLine();
+            
+            string employeeName = CheckName();
 
 
             //Display employee name
@@ -83,7 +136,7 @@ namespace WagesApp
 
 
             //Call the CalculateWages()
-            CalculateWages(sumHoursWorked);
+            CalculateWages(sumHoursWorked, employeeName);
 
         }
 
@@ -92,7 +145,15 @@ namespace WagesApp
         //when run or main process
         static void Main(string[] args)
         {
-            OneEmployee();
+            string flagMain = "";
+            while (!flagMain.Equals("XXX"))
+            {
+                OneEmployee();
+
+
+                flagMain = CheckFlag();
+            }
+            Console.WriteLine($"Top Earner: {topEarner} with {topEarnerHours} hours");
         }
     }
 }
